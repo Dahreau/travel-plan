efe8340446f8e997a13cbf9f9556639e7c2857d5
doupe <142741352+Dahreau@users.noreply.github.com>
2026-07-21 08:45:50 +0200

    initialize project structure with backend and frontend services

---
+++ backend/travel-service/src/test/java/com/travel_plan/travel_service/TravelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel_plan.travel_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
